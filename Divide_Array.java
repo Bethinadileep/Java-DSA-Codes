75afab60f28f78b018067c0432e38f923d69c6f0
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-03-21 21:49:40 +0530

    Create Divide_Array.java

---
+++ Divide_Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean divideArray(int[] nums) {
+        if(nums.length%2 != 0) return false;
+        Arrays.sort(nums);
+        for(int i = 0;i<nums.length;){
+            if(nums[i] != nums[i+1]) return false;
+            else {
+                i=i+2;
+            }
+        }
+        return true;
+    }
+}
