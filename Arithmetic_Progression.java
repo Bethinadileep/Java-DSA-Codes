c2c60b7b426ee7f4875c89543c3587ed4b41b417
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-04-24 15:23:49 +0530

    Create Arithmetic_Progression.java

---
+++ Arithmetic_Progression.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		int a = 2;
+		int d = 2;
+		int n = 4;
+		int sum = 0;
+		sum = (n/2)*(2*a + (n - 1)*d);
+		
+		System.out.println(sum);
+	}
+}
