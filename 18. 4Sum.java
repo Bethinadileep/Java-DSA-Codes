5fdf049bb9cc1838b7b0324ba5d8b624a49bbb5b
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-10-18 22:18:31 +0530

    Create 18. 4Sum.java

---
+++ 18. 4Sum.java
@@ -0,0 +1,122 @@
+//Using 3 pointers
+import java.util.*;
+class Solution {
+    public List<List<Integer>> fourSum(int[] num, int target) {
+        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+        if (num == null || num.length == 0)
+            return res;
+        
+        int n = num.length;
+    
+        Arrays.sort(num); 
+    
+        for (int i = 0; i < n; i++) {
+        
+            int target_3 = target - num[i];
+        
+            for (int j = i + 1; j < n; j++) {
+            
+                int target_2 = target_3 - num[j];
+            
+                int front = j + 1;
+                int back = n - 1;
+            
+                while(front < back) {
+                
+                    int two_sum = num[front] + num[back];
+                
+                    if (two_sum < target_2) front++;
+                
+                    else if (two_sum > target_2) back--;
+                
+                    else {
+                    
+                        List<Integer> quad = new ArrayList<>(); 
+                        quad.add(num[i]);
+                        quad.add(num[j]);
+                        quad.add(num[front]);
+                        quad.add(num[back]);
+                        res.add(quad);
+                    
+                        // Processing the duplicates of number 3
+                        while (front < back && num[front] == quad.get(2)) ++front;
+                    
+                        // Processing the duplicates of number 4
+                        while (front < back && num[back] == quad.get(3)) --back;
+                
+                    }
+                }
+                
+                // Processing the duplicates of number 2
+                while(j + 1 < n && num[j + 1] == num[j]) ++j;
+            }
+        
+            // Processing the duplicates of number 1
+            while (i + 1 < n && num[i + 1] == num[i]) ++i;
+        
+        }
+        
+    
+        return res;
+    }
+}
+
+//using Hashset
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        return kSum(nums, target, 0, 4);
+    }
+    
+    public List<List<Integer>> kSum(int[] nums, long target, int start, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        
+        // If we have run out of numbers to add, return res.
+        if (start == nums.length) {
+            return res;
+        }
+        
+        // There are k remaining values to add to the sum. The 
+        // average of these values is at least target / k.
+        long average_value = target / k;
+        
+        // We cannot obtain a sum of target if the smallest value
+        // in nums is greater than target / k or if the largest 
+        // value in nums is smaller than target / k.
+        if  (nums[start] > average_value || average_value > nums[nums.length - 1]) {
+            return res;
+        }
+        
+        if (k == 2) {
+            return twoSum(nums, target, start);
+        }
+    
+        for (int i = start; i < nums.length; ++i) {
+            if (i == start || nums[i - 1] != nums[i]) {
+                for (List<Integer> subset : kSum(nums, target - nums[i], i + 1, k - 1)) {
+                    res.add(new ArrayList<>(Arrays.asList(nums[i])));
+                    res.get(res.size() - 1).addAll(subset);
+                }
+            }
+        }
+    
+        return res;
+    }
+        
+    public List<List<Integer>> twoSum(int[] nums, long target, int start) {
+        List<List<Integer>> res = new ArrayList<>();
+        Set<Long> s = new HashSet<>();
+    
+        for (int i = start; i < nums.length; ++i) {
+            if (res.isEmpty() || res.get(res.size() - 1).get(1) != nums[i]) {
+                if (s.contains(target - nums[i])) {
+                    res.add(Arrays.asList((int)target - nums[i], nums[i]));
+                }
+            }
+            s.add((long)nums[i]);
+        }
+                                                  
+        return res;
+    }
+}
